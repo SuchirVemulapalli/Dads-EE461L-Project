2cb88f67c1fdad842f0f68fdc6a51763395b425f
Suchir Vemulapalli <suchirvemulapalli@gmail.com>
2023-11-06 15:21:12 -0600

    Fixed landing page buttons

--- frontend/src/components/IndexNavBar.js
+++ frontend/src/components/IndexNavBar.js
@@ -1,21 +1,54 @@
-import React from "react";
-import "../App.css"
+import React, { useState} from "react";
+import { useLocation } from "react-router-dom"; // Import useLocation from react-router-dom
+import "../App.css";
+
 const IndexNavBar = () => {
+  const location = useLocation(); // Get the current location from react-router-dom
+
+  // Determine the initial active link based on the current route
+  let initialActiveLink = "Login";
+  if (location.pathname === "/create-user") {
+    initialActiveLink = "Create User";
+  } else if (location.pathname === "/tutorial") {
+    initialActiveLink = "Tutorial";
+  }
+
+  const [activeLink, setActiveLink] = useState(initialActiveLink);
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    // Store the active link in local storage
+    localStorage.setItem("activeLink", link);
+  };
+
   return (
     <div>
       <ul className="nav nav-underline">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">
+          <a
+            className={`nav-link ${activeLink === "Login" ? "active" : ""}`}
+            aria-current="page"
+            href="/"
+            onClick={() => handleLinkClick("Login")}
+          >
             Login
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/create-user">
+          <a
+            className={`nav-link ${activeLink === "Create User" ? "active" : ""}`}
+            href="/create-user"
+            onClick={() => handleLinkClick("Create User")}
+          >
             Create User
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/tutorial">
+          <a
+            className={`nav-link ${activeLink === "Tutorial" ? "active" : ""}`}
+            href="/tutorial"
+            onClick={() => handleLinkClick("Tutorial")}
+          >
             Tutorial
           </a>
         </li>
--- frontend/src/components/LandingNavBar.js
+++ frontend/src/components/LandingNavBar.js
@@ -1,21 +1,54 @@
-import React from "react";
-import "../App.css"
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom"; // Import useLocation from react-router-dom
+import "../App.css";
+
 const LandingNavBar = () => {
+  const location = useLocation(); // Get the current location from react-router-dom
+  const [activeLink, setActiveLink] = useState("Projects");
+
+  useEffect(() => {
+    // Determine the initial active link based on the current route
+    if (location.pathname === "/create-project") {
+      setActiveLink("Create Project");
+    } else if (location.pathname === "/join-project") {
+      setActiveLink("Join Project");
+    }
+  }, [location]);
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    // Store the active link in local storage
+    localStorage.setItem("landingActiveLink", link);
+  };
+
   return (
     <div>
       <ul className="nav nav-underline">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/landing">
+          <a
+            className={`nav-link ${activeLink === "Projects" ? "active" : ""}`}
+            aria-current="page"
+            href="/landing"
+            onClick={() => handleLinkClick("Projects")}
+          >
             Projects
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/create-project">
+          <a
+            className={`nav-link ${activeLink === "Create Project" ? "active" : ""}`}
+            href="/create-project"
+            onClick={() => handleLinkClick("Create Project")}
+          >
             Create Project
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/join-project">
+          <a
+            className={`nav-link ${activeLink === "Join Project" ? "active" : ""}`}
+            href="/join-project"
+            onClick={() => handleLinkClick("Join Project")}
+          >
             Join Project
           </a>
         </li>
