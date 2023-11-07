22149eeed66dfe0ea201a1b0702f9983f1c90c6c
Andrew <andrewkim6@utexas.edu>
2023-11-07 16:47:26 -0600

    added vars for error msg

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -7,14 +7,68 @@ const Item = ({ itemName, prop }) => {
 
   const [qty, setQty] = useState(prop.quantity)
   const [projectID, setProjectID] = useState(prop.projectID)
+
+  const [input, setInput] = useState(null)
+  
+  const checkOut = () =>{
+    if (!qty){
+      let text = "error";
+      setShowText(true)
+      setStatus(text)
+    }
+    else{
+      axios
+        .post(
+          "http://127.0.0.1:5000/checkOut",
+          {
+            projectid: projectID,
+            input: input,
+            quantity: qty,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          // Check if the response status is OK (status code 200)
+          if (response.status !== 200) {
+            throw new Error("Network response was not ok");
+          }
+          return response.data; // Axios automatically parses the response data as JSON
+        })
+        .then((data) => {
+          console.log(data)
+          if (data.status === "failure"){
+            let text = "error";
+            setShowText(true)
+            setStatus(text)
+          }
+          else{
+            let projectid = data.projectid;
+            let output = data.output;
+            let text = output + " hardware checked out";
+            setShowText(true)
+            setStatus(text)
+            setRemain(output)
+
+          }
+        })
+        .catch((error) => {
+          // Handle errors, e.g., network errors or API errors
+          console.error("There was a problem with the fetch operation:", error);
+        });
+      }
+  }
   
   return (
     <div>
       <div className='itemRow'>
         <h3 className='itemElement'>{itemName}: {qty}/100</h3>
-        <input type= "number" className='itemElement' placeholder="Enter qty" />
+        <input type= "number" className='itemElement' placeholder="Enter qty" onChange={(e) => setInput(e.target.value)}/>
         <button className='itemElement'>Check In</button>
-        <button className='itemElement'>Check Out</button>
+        <button className='itemElement' onClick={checkOut}>Check Out</button>
       </div>
     </div>
   );
