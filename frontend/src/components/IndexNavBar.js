31ada161d2fa9016c8f6aafe24b215022d55ada7
Suchir Vemulapalli <suchirvemulapalli@gmail.com>
2023-11-29 01:30:29 -0600

    Added User Guide

--- frontend/src/components/IndexNavBar.js
+++ frontend/src/components/IndexNavBar.js
@@ -1,67 +1,78 @@
-import React, { useState} from "react";
-import { useLocation } from "react-router-dom"; // Import useLocation from react-router-dom
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import "../css/App.css";
 
-
 const IndexNavBar = () => {
-  const location = useLocation(); // Get the current location from react-router-dom
+  const location = useLocation();
 
-  // Determine the initial active link based on the current route
   let initialActiveLink = "Login";
   if (location.pathname === "/create-user") {
     initialActiveLink = "Create User";
-  } else if (location.pathname === "/tutorial") {
-    initialActiveLink = "Tutorial";
+  } else if (location.pathname === "/userguide") {
+    initialActiveLink = "User Guide";
   }
 
   const [activeLink, setActiveLink] = useState(initialActiveLink);
 
-  const handleLinkClick = (link) => {
-    setActiveLink(link);
-    // Store the active link in local storage
-    localStorage.setItem("activeLink", link);
+  const handleLinkClick = (link, event) => {
+    // Open link in a new tab if it's the "Tutorial" link
+    if (link === "User Guide") {
+      event.preventDefault();
+      window.open("https://docs.google.com/document/d/1GDG985bTcjLlKcvI-6qtf1ZAPaq5pyeqyiGlA4xJwBQ/edit?usp=sharing", "_blank");
+    } else {
+      // Update state for other links
+      setActiveLink(link);
+      // Store the active link in local storage
+      localStorage.setItem("activeLink", link);
+    }
   };
+
   const linkStyle = {
-    color: 'white',
+    color: "white",
     fontSize: 32,
-    fontStyle: 'bold',
-    padding: 30
+    fontStyle: "bold",
+    padding: 30,
   };
+
   return (
     <div>
       <ul className="nav nav-underline">
         <li className="nav-item">
           <a
             className={`nav-link ${activeLink === "Login" ? "active" : ""}`}
-            color={'white'}
+            color={"white"}
             aria-current="page"
             href="/"
             style={linkStyle}
-            onClick={() => handleLinkClick("Login")}
+            onClick={(event) => handleLinkClick("Login", event)}
           >
             Login
           </a>
         </li>
         <li className="nav-item">
           <a
-            className={`nav-link ${activeLink === "Create User" ? "active" : ""}`}
+            className={`nav-link ${
+              activeLink === "Create User" ? "active" : ""
+            }`}
             href="/create-user"
             style={linkStyle}
-            onClick={() => handleLinkClick("Create User")}
+            onClick={(event) => handleLinkClick("Create User", event)}
           >
             Create User
           </a>
         </li>
-        {/* <li className="nav-item">
+        <li className="nav-item">
           <a
-            className={`nav-link ${activeLink === "Tutorial" ? "active" : ""}`}
-            href="/tutorial"
+            className={`nav-link ${
+              activeLink === "User Guide" ? "active" : ""
+            }`}
+            href="/userguide"
             style={linkStyle}
-            onClick={() => handleLinkClick("Tutorial")}
+            onClick={(event) => handleLinkClick("User Guide", event)}
           >
-            Tutorial
+            User Guide
           </a>
-        </li> */}
+        </li>
       </ul>
     </div>
   );
